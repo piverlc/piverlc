d19a521af3328efdb5c6c52c04e4517b26b12469
piverlc <piverlc.bw@gmail.com>
2023-04-27 22:45:01 +0530

    removed `@typescript-eslint`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,43 +2,9 @@
 const config = {
   $schema: 'https://json.schemastore.org/eslintrc',
   root: true,
-  overrides: [
-    {
-      extends: ['plugin:@typescript-eslint/recommended-requiring-type-checking'],
-      files: ['*.ts', '*.tsx'],
-      parserOptions: {
-        project: 'tsconfig.json',
-      },
-    },
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.json',
-  },
-  plugins: ['@typescript-eslint', 'tailwindcss'],
-  extends: [
-    'next/core-web-vitals',
-    'prettier',
-    'plugin:tailwindcss/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  plugins: ['tailwindcss'],
+  extends: ['prettier', 'next/core-web-vitals', 'plugin:tailwindcss/recommended'],
   rules: {
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/consistent-type-imports': [
-      'warn',
-      {
-        prefer: 'type-imports',
-        fixStyle: 'inline-type-imports',
-      },
-    ],
-    '@typescript-eslint/no-misused-promises': [
-      2,
-      {
-        checksVoidReturn: {
-          attributes: true,
-        },
-      },
-    ],
     'tailwindcss/no-custom-classname': 'off',
   },
   settings: {
