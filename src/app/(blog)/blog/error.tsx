734d2e0d96665e4c213182b774ba1869f50cf51a
piverlc <91478831+piverlc@users.noreply.github.com>
2023-04-28 19:55:36 +0530

    feat/posts (#44)
    * use `kebab-case`
    
    * typefix
    
    * update pnpm-lock
    
    * removed `LayoutGroup` for non-animate layout
    
    - changed to normal functions
    
    * name change
    
    * `loading` ui
    
    * `error` handling
    
    * added types
    
    * added `posts`
    
    - Two Forms of Pre-rendering
    - 'When to Use Static Generation v.s.    Server-side Rendering'
    
    * render all docs
    
    * replaced `ui` components

---
+++ src/app/(blog)/blog/error.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { useEffect } from 'react';
+import { Button } from '~/components/button';
+
+// // TODO: improve as per application grows
+export default function Error({ error, reset }: { error: Error; reset: () => void }) {
+  useEffect(() => {
+    console.log('logging error:', error);
+  }, [error]);
+
+  return (
+    <div className='space-y-4'>
+      <h2 className='text-lg font-bold text-light'>Something went wrong! Error</h2>
+      <p className='text-sm text-light'>{error?.message}</p>
+      <Button onClick={() => reset()} variant='outline'>
+        Try Again
+      </Button>
+    </div>
+  );
+}
