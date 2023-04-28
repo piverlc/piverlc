6feb911a3b4106b3d99df4a3363a60253e2cce1d
piverlc <piverlc.bw@gmail.com>
2023-04-28 19:42:22 +0530

    added `posts`
    - Two Forms of Pre-rendering
    - 'When to Use Static Generation v.s.    Server-side Rendering'

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -2,11 +2,15 @@ import { defineDocumentType, makeSource } from 'contentlayer/source-files';
 
 /** @type {import('contentlayer/source-files').ComputedFields} */
 const computedFields = {
-  slug: {
+  slugDir: {
+    type: 'string',
+    resolve: (doc) => doc._raw.sourceFileDir,
+  },
+  slugAsPath: {
     type: 'string',
     resolve: (doc) => `/${doc._raw.flattenedPath}`,
   },
-  slugAsParams: {
+  slugAsSegments: {
     type: 'string',
     resolve: (doc) => doc._raw.flattenedPath.split('/').slice(1).join('/'),
   },
@@ -36,7 +40,31 @@ export const Example = defineDocumentType(() => ({
   },
 }));
 
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `posts/**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: {
+      type: 'string',
+      required: true,
+    },
+    description: {
+      type: 'string',
+    },
+    date: {
+      type: 'date',
+      required: true,
+    },
+    author: {
+      type: 'string',
+      default: '@Piverlc',
+    },
+  },
+  computedFields,
+}));
+
 export default makeSource({
   contentDirPath: 'content',
-  documentTypes: [Example],
+  documentTypes: [Example, Post],
 });
