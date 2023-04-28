17a602790b9d45723a6a0726cea1081e2b868b21
piverlc <piverlc.bw@gmail.com>
2023-04-28 19:39:09 +0530

    `loading` ui

---
+++ src/components/loading-skeleton.tsx
@@ -0,0 +1,8 @@
+// TODO: improve as per application grows
+export default function LoadingSkeleton() {
+  return (
+    <div className='space-y-4'>
+      <h1 className='text-xl font-medium text-gray-400/80'>Loading...</h1>
+    </div>
+  );
+}
