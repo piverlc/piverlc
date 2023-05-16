2c2aee316fffe65f2d4806cb81cacb78ce694744
piverlc <piverlc.bw@gmail.com>
2023-05-16 23:20:38 +0530

    ui components refactor - added Slot to button - changed to inline export - icons import change - noErrorTruncation enabled in tsconfig

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,10 +1,11 @@
 'use client';
 
+import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
-import * as React from 'react';
+import { forwardRef } from 'react';
 import { cn } from '~/utils/cn';
 
-const buttonVariants = cva(
+export const buttonVariants = cva(
   'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 dark:hover:bg-slate-800 dark:hover:text-slate-100 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900 data-[state=open]:bg-slate-100 dark:data-[state=open]:bg-slate-800',
   {
     variants: {
@@ -35,12 +36,15 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {}
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, ...props }, ref) => {
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button';
     return (
-      <button
+      <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
@@ -49,5 +53,3 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   }
 );
 Button.displayName = 'Button';
-
-export { Button, buttonVariants };
--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,4 +1,7 @@
 import {
+  CheckIcon,
+  ChevronRightIcon,
+  CircleIcon,
   GitHubLogoIcon,
   LaptopIcon,
   MoonIcon,
@@ -12,4 +15,7 @@ export const Icons = {
   laptop: LaptopIcon,
   moon: MoonIcon,
   sun: SunIcon,
+  check: CheckIcon,
+  chevronRight: ChevronRightIcon,
+  circle: CircleIcon,
 };
