ff73e6e9e3daaa01dd5051cb9290a9b80984445f
piverlc <piverlc.bw@gmail.com>
2023-10-24 20:15:10 +0530

    added heading to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default async function Home() {
-  return null;
+  return (
+    <section>
+      <h1 className='mb-8 text-2xl font-bold tracking-tighter'>hello there,😀</h1>
+    </section>
+  );
 }
